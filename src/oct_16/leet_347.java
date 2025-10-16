f35bf5986782e3988f6610ef0479ef28b880d00a
Abhinav <abhinaaavvv07187@gmail.com>
2025-10-16 19:29:36 +0530

    DAY 6 - OCT 16

---
+++ src/oct_16/leet_347.java
@@ -0,0 +1,25 @@
+package oct_16;
+
+import java.util.*;
+
+public class leet_347 {
+    public static int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        for (int num : nums) freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        PriorityQueue<Map.Entry<Integer, Integer>> minHeap = new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
+        for (Map.Entry<Integer, Integer> entry : freqMap.entrySet()) {
+            minHeap.add(entry);
+            if (minHeap.size() > k) minHeap.poll();
+        }
+        int[] result = new int[k];
+        for (int i = k - 1; i >= 0; i--) result[i] = minHeap.poll().getKey();
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 1, 2, 2, 3};
+        int k = 2;
+        int[] res = topKFrequent(nums, k);
+        System.out.println(Arrays.toString(res));
+    }
+}
