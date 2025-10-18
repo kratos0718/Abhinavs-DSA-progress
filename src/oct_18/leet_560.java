bc7e6c913c2b8593497761077dd4490f09cd3575
Abhinav <abhinaaavvv07187@gmail.com>
2025-10-18 12:26:41 +0530

    DAY 8 - OCT 18

---
+++ src/oct_18/leet_560.java
@@ -0,0 +1,23 @@
+package oct_18;
+
+import java.util.*;
+
+public class leet_560 {
+    public static int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int sum = 0, count = 0;
+        for (int num : nums) {
+            sum += num;
+            if (map.containsKey(sum - k)) count += map.get(sum - k);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        int k = 3;
+        System.out.println(subarraySum(nums, k));
+    }
+}
